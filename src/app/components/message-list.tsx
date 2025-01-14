cecb2a000cc24d059ef7de88b9258336ff794b21
mimccio <mimccio.dev@gmail.com>
2025-01-14 16:50:02 +0100

    message item responsive width

--- src/app/components/message-list.tsx
+++ src/app/components/message-list.tsx
@@ -15,7 +15,7 @@ export const MessageList = ({ messageList, isLoading }: Props) => {
         {messageList.map((msg, i) => (
           <li className={`flex ${msg.isBot ? '' : 'justify-end'}`} key={i}>
             <span
-              className={`flex flex-col gap-1 rounded-lg p-2 ${msg.isBot ? 'bg-neutral-50 text-neutral-700' : 'bg-indigo-100 text-indigo-700'}`}
+              className={`xs:max-w-[90%] flex flex-col gap-1 rounded-lg px-3 py-2 shadow-sm md:max-w-[70%] ${msg.isBot ? 'bg-neutral-50 text-neutral-700' : 'bg-indigo-100 text-indigo-700'}`}
             >
               {msg.text}
             </span>
