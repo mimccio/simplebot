6e662d6974765b3dcbfccb641342d157e954b8a2
mimccio <mimccio.dev@gmail.com>
2025-01-14 17:42:07 +0100

    empty list message

--- src/app/components/message-list.tsx
+++ src/app/components/message-list.tsx
@@ -1,6 +1,7 @@
 import type { RefObject } from 'react'
 import { format } from 'date-fns'
 import SyncLoader from 'react-spinners/SyncLoader'
+import { BotMessageSquare } from 'lucide-react'
 
 import { ScrollArea } from '@/common/ui/scroll-area'
 import { Message } from '../types/message'
@@ -15,9 +16,21 @@ export const MessageList = ({ listRef, messageList, isLoading }: Props) => {
   return (
     <ScrollArea className="xs:px-4 xs:h-[calc(100svh-176px)] h-[calc(100svh-220px)] border-y border-indigo-200 bg-white px-2 sm:mx-4 sm:rounded-md sm:border sm:px-4">
       <ul ref={listRef} className="flex flex-col gap-2">
-        <span className="py-2 text-center text-xs font-semibold opacity-20">
-          - Début de la conversation-
-        </span>
+        {messageList.length ? (
+          <span className="py-2 text-center text-xs font-semibold text-neutral-200">
+            - Début de la conversation-
+          </span>
+        ) : (
+          <div className="mt-16 flex justify-center">
+            <span className="flex max-w-xs flex-col items-center gap-4 rounded-lg border-indigo-400 bg-indigo-400 p-4 shadow-md">
+              <BotMessageSquare className="text-indigo-50" />
+              <span className="text-center text-white">
+                Commencez une discution avec SimpleBot en entrant un message
+              </span>
+            </span>
+          </div>
+        )}
+
         {messageList.map((msg, i) => (
           <li className={`flex ${msg.isBot ? '' : 'justify-end'}`} key={i}>
             <span
